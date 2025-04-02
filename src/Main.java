c66b91dcca61ba617e10973f782e80751360315b
nilsedgar <nils.jacobsen@zocom.se>
2025-04-02 09:34:39 +0200

    made a scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Nu har jag en scanner");
     }
 }
